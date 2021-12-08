0e85818951985c03347222f460411ea6809d3def
monu <skmkshiva@gmail.com>
2021-12-08 20:29:41 +0530

    fot test

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
        <>
         <button onClick={Mng}>Manage</button>
         <button onClick={Dasb}>Dashboard</button>
-        <button onClick={fff} >monukr</button>
+        <button onClick={fff} >monuG</button>
        </>
         :null
        }
