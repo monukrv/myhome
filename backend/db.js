b3d3a27881aada71b627286d79b9b15cff33f57d
monu <skmkshiva@gmail.com>
2021-12-16 21:20:57 +0530

    delete

--- backend/db.js
+++
@@ -1,9 +0,0 @@
-const mysql =require('mysql')
-
-module.exports = mysql.createConnection({
-  host              : "localhost",
-  user              : "root",
-  password          : "Abcd@1234",
-  database          : "user_data",
-  multipleStatements:true
-});
