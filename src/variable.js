179f94161a3f46fe321da0b289323f3e00faa4b1
monu <skmkshiva@gmail.com>
2021-12-17 14:54:50 +0530

    changes'

--- src/variable.js
+++ src/variable.js
@@ -1,9 +1,8 @@
 export const urll = {
     /*
-    r
     remoteurl:'http://127.0.0.1:3001',
    
     */
     
-    emoteurl:'http://128.199.16.143:3001', 
+    remoteurl:'http://128.199.16.143:3001', 
  };
